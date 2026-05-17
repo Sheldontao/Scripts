ab1fab51f1a527791234d702aa8ab17b4f253b80
Sheldonato <51hhxxttxsxt@gmail.com>
2026-05-17 12:46:08 +0800

    same

--- xhs/xhs_fmz200.js
+++ xhs/xhs_fmz200.js
@@ -889,17 +889,18 @@ function Env(t, e) {
   }
   return new (class {
     constructor(t, e) {
-      ((this.name = t),
-        (this.http = new s(this)),
-        (this.data = null),
-        (this.dataFile = "box.dat"),
-        (this.logs = []),
-        (this.isMute = !1),
-        (this.isNeedRewrite = !1),
-        (this.logSeparator = "\n"),
-        (this.encoding = "utf-8"),
-        (this.startTime = new Date().getTime()),
-        Object.assign(this, e));
+      (((this.name = t),
+      (this.http = new s(this)),
+      (this.data = null),
+      (this.dataFile = "box.dat"),
+      (this.logs = []),
+      (this.isMute = !1),
+      (this.isNeedRewrite = !1),
+      (this.logSeparator = "\n"),
+      (this.encoding = "utf-8"),
+      (this.startTime = new Date().getTime()),
+      Object.assign(this, e)),
+        this.log(`${this.name}, 开始! 🕛 ${this.startTime}`));
     }
     isNode() {
       return "undefined" != typeof module && !!module.exports;
